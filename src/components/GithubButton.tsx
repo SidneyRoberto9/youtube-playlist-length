5c6c8989d3c2a629060520e23115e72916ab64b3
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-11-03 01:44:32 -0300

    ✨ feat: Add Github button

---
+++ src/components/GithubButton.tsx
@@ -0,0 +1,9 @@
+import { Github } from 'lucide-react';
+
+export function GithubButton() {
+  return (
+    <a href="https://github.com/SidneyRoberto9" target="_blank" className="p-2 ">
+      <Github className="h-5 w-5" />
+    </a>
+  );
+}
